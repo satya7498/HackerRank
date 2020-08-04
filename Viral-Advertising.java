5d4ec9e2681203b1001dc2ca065b69758fdf052c
Satya Rao <48786016+satya7498@users.noreply.github.com>
2020-08-04 23:22:07 +0530

    Viral Advertising

---
+++ Viral-Advertising.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the viralAdvertising function below.
+    static int viralAdvertising(int n) {
+        int shared=5;
+        int cum=0;
+        int liked;
+  for(int i=1;i<=n;i++)
+  {
+      liked=(int)Math.floor(shared/2);
+      cum=cum+liked;
+      shared=liked*3;
+  }
+  return cum;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int result = viralAdvertising(n);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
